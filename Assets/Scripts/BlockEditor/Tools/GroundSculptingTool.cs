7a87a9494c06386f41e10e593d58267ffc65da9a
p-svacha <philip.svacha@gmail.com>
2024-03-13 23:15:22 +0100

    added smooth edge option to ground sculpting tool

--- Assets/Scripts/BlockEditor/Tools/GroundSculptingTool.cs
+++ Assets/Scripts/BlockEditor/Tools/GroundSculptingTool.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace WorldEditor
 {
@@ -14,6 +15,9 @@ namespace WorldEditor
         public override Sprite Icon => ResourceManager.Singleton.GroundSculptingSprite;
 
         private int AreaSize = 1;
+
+        [Header("Elements")]
+        public Toggle SmoothEdgeToggle;
 
         public override void UpdateTool()
         {
@@ -54,26 +58,79 @@ namespace WorldEditor
 
         public override void HandleLeftClick()
         {
-            if (AreaSize == 1) // Single tile height change
+            ApplyHeightChange(isIncrease: true);
+        }
+
+        public override void HandleRightClick()
+        {
+            ApplyHeightChange(isIncrease: false);
+        }
+        
+        private void ApplyHeightChange(bool isIncrease)
+        {
+            if (AreaSize == 1 && World.NodeHoverMode9 != Direction.None) // Partial height change of single tile
             {
-                if (World.HoveredGroundNode != null && World.CanChangeHeight(World.HoveredGroundNode, World.NodeHoverMode9, isIncrease: true))
+                if (World.HoveredGroundNode != null && World.CanChangeHeight(World.HoveredGroundNode, World.NodeHoverMode9, isIncrease))
                 {
-                    World.ChangeHeight(World.HoveredGroundNode, World.NodeHoverMode9, isIncrease: true);
+                    World.ChangeHeight(World.HoveredGroundNode, World.NodeHoverMode9, isIncrease);
                 }
             }
-            else // Multitile height change
+            else // Height change of full tile(s)
             {
                 if (World.HoveredGroundNode != null)
                 {
-                    List<GroundNode> nodesInArea = World.GetGroundNodes(World.HoveredGroundNode.WorldCoordinates, AreaSize, AreaSize);
+                    Vector2Int coordinates = World.HoveredGroundNode.WorldCoordinates;
+                    List<GroundNode> nodesInArea = World.GetGroundNodes(coordinates, AreaSize, AreaSize);
 
-                    int minHeight = nodesInArea.Min(x => x.BaseHeight);
+                    List<GroundNode> affectedNodes = new List<GroundNode>();
 
-                    List<GroundNode> affectedNodes = nodesInArea.Where(x => x.BaseHeight == minHeight && x.CanChangeHeight(Direction.None, isIncrease: true)).ToList();
+                    if(isIncrease)
+                    {
+                        int minHeight = nodesInArea.Min(x => x.BaseHeight);
+                        affectedNodes = nodesInArea.Where(x => x.BaseHeight == minHeight && x.CanChangeHeight(Direction.None, isIncrease)).ToList();
+                    }
+                    else
+                    {
+                        int maxHeight = nodesInArea.Max(x => x.MaxHeight);
+                        affectedNodes = nodesInArea.Where(x => x.MaxHeight == maxHeight && x.CanChangeHeight(Direction.None, isIncrease: false)).ToList();
+                    }
 
                     foreach (GroundNode node in affectedNodes)
                     {
-                        node.ChangeHeight(Direction.None, isIncrease: true);
+                        node.ChangeHeight(Direction.None, isIncrease);
+                    }
+
+                    // Smooth outside edges
+                    if (SmoothEdgeToggle.isOn)
+                    {
+                        // North side
+                        for (int x = 0; x < AreaSize; x++)
+                        {
+                            GroundNode node = World.GetGroundNode(new Vector2Int(coordinates.x + x, coordinates.y + AreaSize - 1));
+                            if (!affectedNodes.Contains(node)) continue;
+                            SmoothNodeEdge(node, Direction.N, isIncrease);
+                        }
+                        // East side
+                        for (int y = 0; y < AreaSize; y++)
+                        {
+                            GroundNode node = World.GetGroundNode(new Vector2Int(coordinates.x + AreaSize - 1, coordinates.y + y));
+                            if (!affectedNodes.Contains(node)) continue;
+                            SmoothNodeEdge(node, Direction.E, isIncrease);
+                        }
+                        // South side
+                        for (int x = 0; x < AreaSize; x++)
+                        {
+                            GroundNode node = World.GetGroundNode(new Vector2Int(coordinates.x + x, coordinates.y));
+                            if (!affectedNodes.Contains(node)) continue;
+                            SmoothNodeEdge(node, Direction.S, isIncrease);
+                        }
+                        // West side
+                        for (int y = 0; y < AreaSize; y++)
+                        {
+                            GroundNode node = World.GetGroundNode(new Vector2Int(coordinates.x, coordinates.y + y));
+                            if (!affectedNodes.Contains(node)) continue;
+                            SmoothNodeEdge(node, Direction.W, isIncrease);
+                        }
                     }
 
                     // Manually update world stuff in one step instead of after each node to increase performance
@@ -84,36 +141,24 @@ namespace WorldEditor
             }
         }
 
-        public override void HandleRightClick()
+        /// <summary>
+        /// Smooths out the edge outside one side of a node
+        /// </summary>
+        private void SmoothNodeEdge(GroundNode node, Direction dir, bool isIncrease)
         {
-            if (AreaSize == 1) // Single tile height change
-            {
-                if (World.HoveredGroundNode != null && World.CanChangeHeight(World.HoveredGroundNode, World.NodeHoverMode9, isIncrease: false))
-                {
-                    World.ChangeHeight(World.HoveredGroundNode, World.NodeHoverMode9, isIncrease: false);
-                }
-            }
-            else // Multitile height change
-            {
-                if (World.HoveredGroundNode != null)
-                {
-                    List<GroundNode> nodesInArea = World.GetGroundNodes(World.HoveredGroundNode.WorldCoordinates, AreaSize, AreaSize);
+            Direction preDir = HelperFunctions.GetPreviousDirection8(dir);
+            Direction preDir_Opp = HelperFunctions.GetOppositeDirection(preDir);
+            Direction postDir = HelperFunctions.GetNextDirection8(dir);
+            Direction postDir_Opp = HelperFunctions.GetOppositeDirection(postDir);
 
-                    int maxHeight = nodesInArea.Max(x => x.MaxHeight);
+            GroundNode adjNodePre = World.GetAdjacentGroundNode(node, preDir);
+            GroundNode adjNodeFull = World.GetAdjacentGroundNode(node, dir);
+            GroundNode adjNodePost = World.GetAdjacentGroundNode(node, postDir);
 
-                    List<GroundNode> affectedNodes = nodesInArea.Where(x => x.MaxHeight == maxHeight && x.CanChangeHeight(Direction.None, isIncrease: false)).ToList();
-
-                    foreach (GroundNode node in affectedNodes)
-                    {
-                        node.ChangeHeight(Direction.None, isIncrease: false);
-                    }
-
-                    // Manually update world stuff in one step instead of after each node to increase performance
-                    World.UpdateNavmeshAround(World.HoveredGroundNode.WorldCoordinates, AreaSize, AreaSize);
-                    World.RedrawNodesAround(World.HoveredGroundNode.WorldCoordinates, AreaSize, AreaSize);
-                    World.UpdateVisionOfNearbyEntitiesDelayed(World.HoveredGroundNode.GetCenterWorldPosition(), AreaSize, AreaSize);
-                }
-            }
+            if (adjNodePre != null && adjNodePre.Height[preDir_Opp] != node.Height[preDir]) adjNodePre.ChangeHeight(preDir_Opp, isIncrease);
+            if (adjNodeFull != null && adjNodeFull.Height[postDir_Opp] != node.Height[postDir]) adjNodeFull.ChangeHeight(postDir_Opp, isIncrease);
+            if (adjNodeFull != null && adjNodeFull.Height[preDir_Opp] != node.Height[preDir]) adjNodeFull.ChangeHeight(preDir_Opp, isIncrease);
+            if (adjNodePost != null && adjNodePost.Height[postDir_Opp] != node.Height[postDir]) adjNodePost.ChangeHeight(postDir_Opp, isIncrease);
         }
 
         public override void OnHoveredGroundNodeChanged(GroundNode oldNode, GroundNode newNode)
